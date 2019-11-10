e96fb10294422a12b4a9db689f507077129824c8
jsa275 <jsa275@sfu.ca>
2019-11-10 04:52:10 -0800

    Animate about image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import tenor from '../../../public/images/tenor.gif'
+import giphy from '../../../public/images/giphy.gif'
 import Image from 'react-bootstrap/Image'
 import './style.css'
 
@@ -8,8 +9,12 @@ export default class About extends Component {
         return (
             <div className="about">
                 <div className="about-image-div">
-                    <Image src={tenor} roundedCircle className="about-image"/>
+                    <Image roundedCircle className="about-image"
+                    // onMouseOver={e => (e.currentTarget.src = giphy)}
+                    // onMouseOut={e => (e.currentTarget.src = tenor)}
+                    />
                 </div>
+        
                 <div className="about-info">
                     <h4>Jackson Situ</h4>
                     <h6>Cat</h6>
