1c5f73196c53d81adbae636700edfb1ad12eb04f
jsa275 <jsa275@sfu.ca>
2019-11-10 17:51:19 -0800

    welcome

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,6 +8,10 @@ export default class About extends Component {
     render () {
         return (
             <div className="about">
+                <div className="about-welcome">
+                    <h6>Welcome to My Page :)</h6>
+                    <h6>It's a work in progress!</h6>
+                </div>
                 <div className="about-image-div">
                     <Image roundedCircle className="about-image"
                     // onMouseOver={e => (e.currentTarget.src = giphy)}
@@ -16,7 +20,7 @@ export default class About extends Component {
                 </div>
         
                 <div className="about-info">
-                    <h4>Jackson Situ</h4>
+                    <h4 style={{fontWeight: "bold"}}>Jackson Situ</h4>
                     <h6>Nerd</h6>
                     <h6>SFU</h6>
                 </div>
